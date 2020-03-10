ae57e4e0cca34794900b5a0689e54f6baa28ecb4
foxjazz <fox21@foxjazz.net>
2020-03-09 20:22:39 -0600

    test for update pattern

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {Data} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  toDisplay: boolean;
   title = 'app';
+
+  constructor(private dd: Data) {
+    this.toDisplay = false;
+  }
+  ngOnInit(): void {
+    this.dd.notifyToDisplay.subscribe(a => {
+      this.toDisplay = a;
+    });
+  }
+  ngOnDestroy(): void {
+    this.dd.notifyToDisplay.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BookComponent } from './book/book.component';
+import { SectionComponent } from './section/section.component';
+import {DataService} from './data.service';
+import {FormsModule} from '@angular/forms';
+import { DisplayComponent } from './display/display.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    SectionComponent,
+    DisplayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
